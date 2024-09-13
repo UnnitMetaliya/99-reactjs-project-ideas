42708d8e013a20cb02975df7a6c89ca86318fdb1
Soapman-PC <jelinsksk@gmail.com>
2024-09-13 10:34:19 +0300

    set up temperature switcher functionality

--- 009_weather_app/project-files/src/components/MainScreenComponents/MainScreen.tsx
+++ 009_weather_app/project-files/src/components/MainScreenComponents/MainScreen.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
+
 import './MainScreen.css'
+
 import TopBarInfo from './TopBarInfo/TopBarInfo'
 
 const MainScreen = () => {
+    const [temp, setTemp] = useState(0)
+
+    let currentTemperature = 23;
+
     return (
         <div className='main_screen__container'>
             {/** TopBarInfo */}
-            <TopBarInfo />
+            <TopBarInfo temperature={temp} setTemperature={setTemp}/>
             {/** CurrentInfo */}
-
+            <div className='main_screen__current_info'>
+                <h1>{temp == 0 ? currentTemperature * (9/5) + 32 : currentTemperature}</h1>
+                <p>°C</p>
+            </div>
             {/** WeeklyInfo */}
             
         </div>
--- 009_weather_app/project-files/src/components/MainScreenComponents/TopBarInfo/TopBarInfo.tsx
+++ 009_weather_app/project-files/src/components/MainScreenComponents/TopBarInfo/TopBarInfo.tsx
@@ -1,13 +1,11 @@
 import './TopBarInfo.css'
-import { useState } from 'react'
 import WeatherIcon from '../../../assets/weather_icons/WeatherIcon - 2-58.png'
 
-const TopBarInfo = () => {
-    const [active_switcher, setActiveSwitcher] = useState(0)
+const TopBarInfo = (props: any) => {
+    
 
     const handleSwitcherClick = (index: number) => {
-        setActiveSwitcher(index)
-        console.log(active_switcher)
+        props.setTemperature(index)
     }
 
     return (
@@ -18,10 +16,10 @@ const TopBarInfo = () => {
             </div>
             {/** Temperature Switch */}
             <div className='top_bar_info__temp_switch'>
-                <a href="#" className={'top_bar_info__temp_switch_switcher' +  (active_switcher == 0 ? 'active_switcher' : '')} onClick={() => handleSwitcherClick(0)}>
+                <a href="#" className={'top_bar_info__temp_switch_switcher' +  (props.temperature == 0 ? 'active_switcher' : '')} onClick={() => handleSwitcherClick(0)}>
                     F
                 </a>
-                <a href="#" className={'top_bar_info__temp_switch_switcher' +  (active_switcher == 0 ? 'active_switcher' : '')} onClick={() => handleSwitcherClick(1)}>
+                <a href="#" className={'top_bar_info__temp_switch_switcher' +  (props.temperature == 0 ? 'active_switcher' : '')} onClick={() => handleSwitcherClick(1)}>
                     C
                 </a>
             </div>
